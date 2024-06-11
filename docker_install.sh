07f40f086e5824a3e612883b0abc38a30adb5f10
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-06-11 14:42:38 +0200

    done

--- docker_install.sh
+++ docker_install.sh
@@ -8,7 +8,7 @@ if [ ! -e "/swapfile" ]; then
     echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 fi
 sudo apt update -y
-sudo apt install screen iftop mtr net-tools python3 python3-pip apt-transport-https ca-certificates curl gnupg lsb-release docker-compose docker.io -y
+sudo apt install zip unzip screen iftop mtr net-tools python3 python3-pip apt-transport-https ca-certificates curl gnupg lsb-release docker-compose docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo lsb_release -a
