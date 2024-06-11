a76ad6c8068634ded01dd80655c1474e4e6b2393
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-06-11 15:05:00 +0200

    done

--- pipinstall.sh
+++ pipinstall.sh
@@ -1,4 +1,4 @@
-sudo python.exe -m pip install --upgrade pip
+sudo python -m pip install --upgrade pip
 sudo pip install --upgrade pip
 sudo pip install python-wordpress-xmlrpc
 # sudo pip install --upgrade --force-reinstall python-wordpress-xmlrpc
