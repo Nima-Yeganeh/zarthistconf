3deb08aa63027b6b47a0330e607f5b516b224eae
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-06-11 14:27:09 +0000

    done

--- pipinstall.sh
+++ pipinstall.sh
@@ -2,4 +2,6 @@ sudo python3 -m pip install --upgrade pip
 sudo pip install --upgrade pip
 sudo pip install python-wordpress-xmlrpc
 # sudo pip install --upgrade --force-reinstall python-wordpress-xmlrpc
-sudo pip install openai
+# sudo pip install openai
+sudo pip install openai==0.28
+
--- test8_api_test.py
+++ test8_api_test.py
@@ -1,10 +1,12 @@
 import openai
 
 # xcode = input("What is the code? ")
-question = input("What is your question? ")
+# question = input("What is your question? ")
+question = "Tell me about Art History"
 # print("Code: " + xcode + "")
 # openai.api_key = "sk-"+xcode+"joeRLSZjsL9bOXI2PT3BlbkFJEc4ys7pAJe7SL82uqxtE"
 openai.api_key = "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"
+openai.api_key = "sk-proj-bEy7fyvHoYSHadFgfHGOT3BlbkFJkQPDWGBXnauIGkm2wGgT"
 
 def generate_content(topic):
     response = openai.ChatCompletion.create(
@@ -20,3 +22,4 @@ def generate_content(topic):
     return(result)
 
 print(generate_content(question))
+
