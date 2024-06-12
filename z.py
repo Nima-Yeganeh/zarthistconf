9133e6fcd045438cc166c1416f60dcb549630bbb
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-06-12 13:19:03 +0200

    done

--- z.py
+++ z.py
@@ -1,34 +1,33 @@
+import re
 import wordpress_xmlrpc
 from wordpress_xmlrpc import Client, WordPressPost
 from wordpress_xmlrpc.methods.posts import NewPost
 
-# WordPress credentials and URL
 url = 'http://arthist.ir/xmlrpc.php'
 username = 'newuser'
 password = 'newuser'
 
-# Create a client instance
 client = Client(url, username, password)
 
-# Create a new post instance
 post = WordPressPost()
-post.title = 'Title 1239'
+post.title = 'Title'
 
-# Read the content of the file and store it in post.content
 with open('zxyz', 'r') as file:
     post_content = file.read()
 post.content = post_content
 
-# Add tags and categories
+title_pattern = r'<title>(.*?)<\/title>'
+match = re.search(title_pattern, file_content)
+post.title = match.group(1)
+
 tags = ['tag1', 'tag2', 'tag3']
 post.terms_names = {
     'post_tag': tags,
     'category': ['category_name']
 }
 
-# Set the post status
 post.post_status = 'publish'
 
-# Publish the post
 post_id = client.call(NewPost(post))
 print('New post created with ID:', post_id)
+
