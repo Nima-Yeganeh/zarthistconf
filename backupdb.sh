36f31628674532938e6c02783d47a54508932ea9
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-06-12 13:35:19 +0000

    done

--- backupdb.sh
+++ backupdb.sh
@@ -2,7 +2,7 @@ sudo rm -f /home/adrian/zarthistweb/zwordpressdb
 sudo docker exec -t $(sudo docker ps -a | grep 'mysql:5.7' | awk '{print $1}') mysqldump -uwordpress -pyour_wordpress_password wordpress > ./wordpress_db_backup.sql && sudo zip --password sudodockerpsexectgrepawkmysqldumpword /home/adrian/zarthistweb/zwordpressdb ./wordpress_db_backup.sql && rm ./wordpress_db_backup.sql
 sudo mv -f /home/adrian/zarthistweb/zwordpressdb.zip /home/adrian/zarthistweb/zwordpressdb
 cd /home/adrian/zarthistweb/
-bash gitupdate.sh
+sudo bash gitupdate.sh
 cd /home/adrian/zarthistconf/
-bash gitupdate.sh
+sudo bash gitupdate.sh
 
