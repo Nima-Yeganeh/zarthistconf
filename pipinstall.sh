ca4fe5ba789601bafbf449582c33a92d2f0f7ff1
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-06-12 13:38:29 +0000

    done

--- pipinstall.sh
+++ pipinstall.sh
@@ -4,4 +4,5 @@ sudo pip install python-wordpress-xmlrpc
 # sudo pip install --upgrade --force-reinstall python-wordpress-xmlrpc
 # sudo pip install openai
 sudo pip install openai==0.28
+sudo pip install bs4
 
