08c1ee89075d39aa825561c9bef9f98080038948
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-06-20 10:48:13 +0000

    done

---
+++ preinstallscript.sh
@@ -0,0 +1,11 @@
+sudo apt-get update -y
+sudo apt install python3 python3-pip -y
+sudo apt-get install imagemagick -y
+# convert --version
+sudo pip install --upgrade python-tgpt
+# pip install --upgrade "python-tgpt[cli]"
+# pip install  --upgrade "python-tgpt[all]"
+# sudo apt-get update -y
+# sudo apt-get install ffmpeg -y
+
+
