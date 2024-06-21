21e07825fd6354a7909ab9c4054dbd92e9cc75cd
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-06-21 14:24:32 +0200

    done

--- ztestfolder/post_prompt_plus_image_plus_mp3_v2.sh
+++ ztestfolder/post_prompt_plus_image_plus_mp3_v2.sh
@@ -1,113 +1,4 @@
 #!/bin/bash
-
-# Define the function
-post_it() {
-
-    local string_to_print="$1"
-    input="$string_to_print"
-
-    echo "Info in English..."
-
-    echo "" > zprompt.txt
-    python3 -m pytgpt generate "in english tell me about $input" > zprompt.txt
-
-    sed -i -e 's/### //g' -e 's/– \*\*//g' -e 's/\*\*//g' -e 's/#//g' zprompt.txt
-    cat zprompt.txt | sed 's/- //g' > zprompt2.txt
-    cat zprompt2.txt > zprompt.txt
-    rm -f zprompt2.txt
-    mv -f zprompt.txt zprompt_info_en
-
-    echo "Translation to French..."
-
-    echo "" > zprompt.txt
-    python3 -m pytgpt generate "translate to french" > zprompt.txt
-
-    sed -i -e 's/### //g' -e 's/– \*\*//g' -e 's/\*\*//g' -e 's/#//g' zprompt.txt
-    cat zprompt.txt | sed 's/- //g' > zprompt2.txt
-    cat zprompt2.txt > zprompt.txt
-    rm -f zprompt2.txt
-    mv -f zprompt.txt zprompt_info_fr
-
-    echo "Check Title..."
-
-    ztitle_fr=$(python3 -m pytgpt generate "Give me a title for this article in french")
-    ztitle_en=$(python3 -m pytgpt generate "Translate this title to english")
-    ztitle="$ztitle_fr | $ztitle_en"
-    # echo $ztitle_en
-    # echo $ztitle_fr
-    echo $ztitle
-
-    echo "Wordpress Tags..."
-
-    python3 -m pytgpt generate "Give me list of wordpress keyword tags for this article in bullet point in french and english" > zwptagstempfile
-    cat zwptagstempfile | grep - | sed 's/- //g' | sed 's/\*\*//g' > zwptags
-    # rm -f zwptagstempfile
-
-    echo "Google Images..."
-
-    echo "" > googleimghtmlfile
-    googleimgurl=$(bash googleimgv2.sh "$input")
-    googleimghtml='<!DOCTYPE html><html><body><p>Click the link to search <a href="'$googleimgurl'">Google Images</a></p><br></body></html>'
-    echo $googleimghtml > googleimghtmlfile
-
-    # echo "Books..."
-
-    # python3 -m pytgpt generate "Give me list of books for this article in bullet point in french and english" > zbookstempfile
-    # cat zbookstempfile | sed 's/\*\*//g' | sed 's/### //g' > zbooks
-
-    # sleep 1000
-
-    zup1=$(cat zupinfo1 | head -n 1)
-    zup2=$(cat zupinfo2 | head -n 1)
-    zup3=$(cat zupinfo3 | head -n 1)
-
-    echo "Downloading Image..."
-
-    python3 imagedl.py "$input"
-
-    echo "Checking JPG Files..."
-
-    file_list=$(ls -anp | grep "$input" | grep jpeg | head -n1)
-
-    filename=$(echo "$file_list" | awk '{for (i=9; i<=NF; i++) printf $i " "; print ""}' | sed 's/ *$//')
-    newfilename=$(date +%Y%m%d%H%M%S%N | md5sum | cut -d ' ' -f 1)
-    extension=".jpg"
-    newfilename="${newfilename}${extension}"
-    echo "New filename: $newfilename"
-    echo "$filename"
-    convert "$filename" -gravity North -chop 0x60 -gravity South -chop 0x60 $newfilename
-    rm -f "$filename"
-
-    echo "MP3 in French..."
-
-    mp3file=$(date +%Y%m%d%H%M%S%N | md5sum | cut -d ' ' -f 1)
-    mp3file="${mp3file}.mp3"
-    python3 ztr2.py "zprompt_info_fr" "$mp3file" "fr"
-
-    echo "MP3 in English..."
-
-    mp3file2=$(date +%Y%m%d%H%M%S%N | md5sum | cut -d ' ' -f 1)
-    mp3file2="${mp3file2}.mp3"
-    python3 ztr2.py "zprompt_info_en" "$mp3file2" "en"
-
-    echo "Wordpress Post..."
-
-    python3 test4_post_plus_image.py "$ztitle" "$newfilename" "$mp3file" "zprompt_info_fr" "$mp3file2" "zprompt_info_en" "zwptags" "$zup1" "$zup2" "$zup3" "googleimghtmlfile"
-    rm -f $newfilename
-    rm -f $mp3file
-    rm -f $mp3file2
-
-    rm -f zprompt_info_en
-    rm -f zprompt_info_fr
-    rm -f zwptagstempfile
-    rm -f zwptags
-    rm -f zbookstempfile
-    rm -f zbooks
-    rm -f googleimghtmlfile
-
-    echo "Done!"
-}
-
 # Read the file line by line
 while IFS= read -r linetext
 do
@@ -116,7 +7,111 @@ do
   echo $linetext
   # Call the function and pass a string
   # post_it "$linetext"
-  python3 ztest.py
+  # python3 ztest.py
+  
+  input=$linetext
+
+  echo "Info in English..."
+
+  echo "" > zprompt.txt
+  python3 -m pytgpt generate "in english tell me about $input" > zprompt.txt
+
+  sed -i -e 's/### //g' -e 's/– \*\*//g' -e 's/\*\*//g' -e 's/#//g' zprompt.txt
+  cat zprompt.txt | sed 's/- //g' > zprompt2.txt
+  cat zprompt2.txt > zprompt.txt
+  rm -f zprompt2.txt
+  mv -f zprompt.txt zprompt_info_en
+
+  echo "Translation to French..."
+
+  echo "" > zprompt.txt
+  python3 -m pytgpt generate "translate to french" > zprompt.txt
+
+  sed -i -e 's/### //g' -e 's/– \*\*//g' -e 's/\*\*//g' -e 's/#//g' zprompt.txt
+  cat zprompt.txt | sed 's/- //g' > zprompt2.txt
+  cat zprompt2.txt > zprompt.txt
+  rm -f zprompt2.txt
+  mv -f zprompt.txt zprompt_info_fr
+
+  echo "Check Title..."
+
+  ztitle_fr=$(python3 -m pytgpt generate "Give me a title for this article in french")
+  ztitle_en=$(python3 -m pytgpt generate "Translate this title to english")
+  ztitle="$ztitle_fr | $ztitle_en"
+  # echo $ztitle_en
+  # echo $ztitle_fr
+  echo $ztitle
+
+  echo "Wordpress Tags..."
+
+  python3 -m pytgpt generate "Give me list of wordpress keyword tags for this article in bullet point in french and english" > zwptagstempfile
+  cat zwptagstempfile | grep - | sed 's/- //g' | sed 's/\*\*//g' > zwptags
+  # rm -f zwptagstempfile
+
+  echo "Google Images..."
+
+  echo "" > googleimghtmlfile
+  googleimgurl=$(bash googleimgv2.sh "$input")
+  googleimghtml='<!DOCTYPE html><html><body><p>Click the link to search <a href="'$googleimgurl'">Google Images</a></p><br></body></html>'
+  echo $googleimghtml > googleimghtmlfile
+
+  # echo "Books..."
+
+  # python3 -m pytgpt generate "Give me list of books for this article in bullet point in french and english" > zbookstempfile
+  # cat zbookstempfile | sed 's/\*\*//g' | sed 's/### //g' > zbooks
+
+  # sleep 1000
+
+  zup1=$(cat zupinfo1 | head -n 1)
+  zup2=$(cat zupinfo2 | head -n 1)
+  zup3=$(cat zupinfo3 | head -n 1)
+
+  echo "Downloading Image..."
+
+  python3 imagedl.py "$input"
+
+  echo "Checking JPG Files..."
+
+  file_list=$(ls -anp | grep "$input" | grep jpeg | head -n1)
+
+  filename=$(echo "$file_list" | awk '{for (i=9; i<=NF; i++) printf $i " "; print ""}' | sed 's/ *$//')
+  newfilename=$(date +%Y%m%d%H%M%S%N | md5sum | cut -d ' ' -f 1)
+  extension=".jpg"
+  newfilename="${newfilename}${extension}"
+  echo "New filename: $newfilename"
+  echo "$filename"
+  convert "$filename" -gravity North -chop 0x60 -gravity South -chop 0x60 $newfilename
+  rm -f "$filename"
+
+  echo "MP3 in French..."
+
+  mp3file=$(date +%Y%m%d%H%M%S%N | md5sum | cut -d ' ' -f 1)
+  mp3file="${mp3file}.mp3"
+  python3 ztr2.py "zprompt_info_fr" "$mp3file" "fr"
+
+  echo "MP3 in English..."
+
+  mp3file2=$(date +%Y%m%d%H%M%S%N | md5sum | cut -d ' ' -f 1)
+  mp3file2="${mp3file2}.mp3"
+  python3 ztr2.py "zprompt_info_en" "$mp3file2" "en"
+
+  echo "Wordpress Post..."
+
+  python3 test4_post_plus_image.py "$ztitle" "$newfilename" "$mp3file" "zprompt_info_fr" "$mp3file2" "zprompt_info_en" "zwptags" "$zup1" "$zup2" "$zup3" "googleimghtmlfile"
+  rm -f $newfilename
+  rm -f $mp3file
+  rm -f $mp3file2
+
+  rm -f zprompt_info_en
+  rm -f zprompt_info_fr
+  rm -f zwptagstempfile
+  rm -f zwptags
+  rm -f zbookstempfile
+  rm -f zbooks
+  rm -f googleimghtmlfile
+
+  echo "Done!"
+
   echo "Next..."
 done < "zztopics.txt"
 
