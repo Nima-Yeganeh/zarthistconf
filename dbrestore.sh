219f9d88a387666a10c516a214af2e937a282852
nima-yeganeh <yeganeh.nima.d@gmail.com>
2024-06-23 17:43:49 +0000

    done

--- dbrestore.sh
+++ dbrestore.sh
@@ -1,5 +1,20 @@
-sudo rm -f wordpress_db_backup.sql
-sudo rm -f zwordpressdb
-cp /root/zarthistweb/zwordpressdb .
-sudo unzip zwordpressdb
+#!/bin/bash
+# Variables
+CONTAINER_NAME="zarthistconf_db_1"
+DATABASE_NAME="wordpress"
+USERNAME="root"
+PASSWORD="your_mysql_password"
+BACKUP_ZIP="wordpress_1234.zip"
+BACKUP_FILE="wordpress_1234.sql"
+BACKFILE="wordpress_1234"
+rm -f $BACKUP_ZIP
+rm -f $BACKUP_FILE
+cp $BACKFILE $BACKUP_ZIP
+docker exec -i "$CONTAINER_NAME" ls -anp /tmp/
+unzip -o $BACKUP_ZIP -d ./
+docker exec -i "$CONTAINER_NAME" mysql -u"$USERNAME" -p"$PASSWORD" "$DATABASE_NAME" < $BACKUP_FILE
+docker exec -i "$CONTAINER_NAME" ls -anp /tmp/
+rm -f $BACKUP_ZIP
+rm -f $BACKUP_FILE
+echo "Restore completed"
 
---
+++ install.sh
@@ -0,0 +1,39 @@
+git pull
+if [ ! -e "/swapfile" ]; then
+    sudo fallocate -l 4G /swapfile
+    ls -anp /swapfile
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+    echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+fi
+sudo apt update -y
+sudo apt install zip unzip screen iftop mtr net-tools python3 python3-pip apt-transport-https ca-certificates curl gnupg lsb-release docker-compose docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo lsb_release -a
+sudo docker ps -a
+sudo python3 -m pip install --upgrade pip
+sudo pip install --upgrade pip
+sudo pip install python-wordpress-xmlrpc
+# sudo pip install --upgrade --force-reinstall python-wordpress-xmlrpc
+# sudo pip install openai
+# sudo pip install openai==0.28
+# sudo pip install bs4
+
+sudo apt-get update -y
+sudo apt install python3 python3-pip -y
+sudo apt-get install imagemagick -y
+# convert --version
+sudo pip install --upgrade python-tgpt
+# pip install --upgrade "python-tgpt[cli]"
+# pip install  --upgrade "python-tgpt[all]"
+# sudo apt-get update -y
+# sudo apt-get install ffmpeg -y
+# sudo apt install python python3-pip -y
+sudo pip install gtts
+ls -lh /root/zarthistweb
+sudo chown -R www-data:www-data /root/zarthistweb/
+sudo chown www-data:www-data /root/zarthistweb/
+ls -lh /root/zarthistweb
+
